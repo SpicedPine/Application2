9011d4e43ad9b140c936ce58c87a8ec4ce7190b3
Igor <igor_noskov_a@mail.ru>
2021-03-31 16:55:37 +0300

    Update multiply_d.h

--- Application2/multiply_d.h
+++ Application2/multiply_d.h
@@ -1,4 +1,5 @@
 #include "omp.h"
+#include "mkl.h"
 
 #define NUM 1025
 void multiply_d(double a[][NUM], double b[][NUM], double c[][NUM])
@@ -23,6 +24,19 @@ void sum_d(double a[][NUM], double b[][NUM], double c[][NUM]) {
 		for (j = 0; j < NUM; j++)
 		{
 			c[i][j] = a[i][j] + b[i][j];
+		}
+	}
+}
+
+multiply_d_mkl(double a[][NUM], double b[][NUM], double c[][NUM]) {
+	int i, j, k;
+#pragma omp parallel for
+	for (i = 0; i < NUM; i++) {
+		for (j = 0; j < NUM; j++) {
+			//2 #pragma omp parallel for
+			for (k = 0; k < NUM; k++) {
+				c[i][j] = c[i][j] + a[i][k] * b[k][j];
+			}
 		}
 	}
 }
