0dafda3d2f567e0e9b1fff0a3b32b9379bc8030e
Igor <igor_noskov_a@mail.ru>
2021-04-07 11:11:06 +0300

    Update multiply_d.h

--- Application2/multiply_d.h
+++ Application2/multiply_d.h
@@ -28,15 +28,26 @@ void sum_d(double a[][NUM], double b[][NUM], double c[][NUM]) {
 	}
 }
 
-multiply_d_mkl(double a[][NUM], double b[][NUM], double c[][NUM]) {
-	int i, j, k;
+void multiply_d_mkl(double a[][NUM], double b[][NUM], double c[][NUM]) {
+	int i, j;
+	for ( i = 0; i < NUM; i++)
+	{
 #pragma omp parallel for
-	for (i = 0; i < NUM; i++) {
-		for (j = 0; j < NUM; j++) {
-			//2 #pragma omp parallel for
-			for (k = 0; k < NUM; k++) {
-				c[i][j] = c[i][j] + a[i][k] * b[k][j];
-			}
+		for (j = 0; j < NUM; j++)
+		{
+			c[j][i] = cblas_ddot(NUM, a[j], 1, b[NUM - i - 1], 1);
+		}
+	}
+
+}
+
+void rotate(double a[][NUM], double b[][NUM]) {
+#pragma omp parallel for
+	for (size_t i = 0; i < NUM; i++)
+	{
+		for (size_t j = 0; j < NUM; j++)
+		{
+			b[NUM - j - 1][i] = a[i][j];
 		}
 	}
 }
