273c2335a170f5f5e965705bb876481ae36712d2
Igor <igor_noskov_a@mail.ru>
2021-04-07 11:11:57 +0300

    Update Application2.cpp

--- Application2/Application2.cpp
+++ Application2/Application2.cpp
@@ -3,7 +3,7 @@
 #include "multiply_d.h"
 #include <time.h> 
 
-static double  a[NUM][NUM], b[NUM][NUM], c[NUM][NUM], a2[NUM][NUM], a2b[NUM][NUM], c2[NUM][NUM], res[NUM][NUM];
+static double  a[NUM][NUM], b[NUM][NUM], c[NUM][NUM], a2[NUM][NUM], a2b[NUM][NUM], c2[NUM][NUM], res[NUM][NUM], a_rot[NUM][NUM], b_rot[NUM][NUM], c_rot[NUM][NUM];
 
 //routine to initialize an array with data
 void init_arr(double row, double col, double off, double a[][NUM])
@@ -45,17 +45,59 @@ int main()
 
 	//start timing the matrix multiply code
 	printf("NUM:%d\n", NUM);
-	//omp_set_num_threads(1);
-	for (size_t i = 0; i < 5; i++)
+	/*for (size_t j = 13; j < 17; j++)
 	{
-		start = clock();
-		multiply_d(a, a, a2);
-		multiply_d(a2, b, a2b);
-		multiply_d(c, c, c2);
-		sum_d(a2b, c2, res);
-		stop = clock();
-		printf("Elapsed time = %lf seconds\n", ((double)(stop - start))/1000);
-	}
+		omp_set_num_threads(j);
+		printf("Number of threads = %d \n\n", j);
+		for (size_t i = 0; i < 5; i++)
+		{
+			printf("Without MKL:");
+			start = clock();
+			multiply_d(a, a, a2);
+			multiply_d(a2, b, a2b);
+			multiply_d(c, c, c2);
+			sum_d(a2b, c2, res);
+			stop = clock();
+			printf("Elapsed time without mkl = %lf seconds\n", ((double)(stop - start)) / 1000);
+
+			printf("With MKL:");
+			start = clock();
+
+			rotate(a, a_rot);
+			multiply_d_mkl(a, a_rot, a2);
+			rotate(b, b_rot);
+			multiply_d_mkl(a2, b_rot, a2b);
+			rotate(c, c_rot);
+			multiply_d_mkl(c, c_rot, c2);
+			sum_d(a2b, c2, res);
+			stop = clock();
+			printf("Elapsed time with mkl = %lf seconds\n", ((double)(stop - start)) / 1000);
+		}
+	}*/
+
+	omp_set_num_threads(16);
+
+	printf("With MKL:");
+	start = clock();
+	rotate(a, a_rot);
+	multiply_d_mkl(a, a_rot, a2);
+	rotate(b, b_rot);
+	multiply_d_mkl(a2, b_rot, a2b);
+	rotate(c, c_rot);
+	multiply_d_mkl(c, c_rot, c2);
+	sum_d(a2b, c2, res);
+	stop = clock();
+	printf("Elapsed time with mkl = %lf seconds\n", ((double)(stop - start)) / 1000);
+
+
+	/*printf("Without MKL:");
+	start = clock();
+	multiply_d(a, a, a2);
+	multiply_d(a2, b, a2b);
+	multiply_d(c, c, c2);
+	sum_d(a2b, c2, res);
+	stop = clock();
+	printf("Elapsed time without mkl = %lf seconds\n", ((double)(stop - start)) / 1000);*/
 
 
 	/*for (size_t j = 1; j < 21; j++)
@@ -79,8 +121,8 @@ int main()
 
 	if (NUM < 5) {
 		print_arr("a", a);
-		print_arr("b", b);
-		print_arr("c", c);
+		print_arr("a", a);
+		print_arr("a^2", a2);
 	}
 
 	// print elapsed time
